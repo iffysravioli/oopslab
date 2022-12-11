08b049faf4a3a1efe5339637f6f66c197a326f05
iffysravioli <111910162+iffysravioli@users.noreply.github.com>
2022-12-11 15:19:55 -0800

    Create student.py

---
+++ student.py
@@ -0,0 +1,10 @@
+class AccessModifiers():
+    def __init__(self, publicInstanceVariable, protectedInstanceVariable, privateInstanceVariable):
+        pass
+
+    def __privateInstanceMethod(self) -> int:
+      pass
+
+    def publicInstanceMethod(self) -> tuple:
+      pass
+    
